8a5ccfbeb34737b33db562f5d952959541ff3dfd
sogdiana56-2 <sogdiana0608@icloud.com>
2025-10-10 20:52:10 +0600

    commit hw

---
+++ main.py
@@ -0,0 +1,49 @@
+import flet as ft
+
+from datetime import datetime 
+
+def main(page: ft.Page):
+   
+   page.title = 'мое первое приложение'
+   page.theme_mode = ft.ThemeMode.LIGHT
+
+   greeting_text = ft.Text("Hello World")
+   name_input = ft.TextField(label='Введите имя:')
+   age_input = ft.TextField(label='Введите возраст')
+   
+   
+   def on_button_click(_):
+      name = name_input.value.strip()
+      age = age_input.value.split()
+      hour = datetime.now().hour 
+
+      if 6 <= hour < 12:
+         greenting = "good morning"
+      elif 12 <= hour < 18:
+         greenting = "good day"
+      elif 18 <= hour < 24:
+         greenting = "good evning"
+      else:
+         greenting = "good night"
+
+      print(name)
+
+      if name:
+         print(greeting_text)
+         greeting_text.value = f'{greenting} {name} {age} лет тебе'
+         print(greeting_text)
+         name_input.value =''
+         age_input.value = ''
+      else:
+         print('не введино')
+         greeting_text.value = 'пж введите имя'
+      page.update()
+
+
+   name_button = ft.ElevatedButton('send', on_click=on_button_click)
+   
+   page.add(greeting_text, name_input, age_input, name_button)
+
+ft.app(target=main)
+
+
